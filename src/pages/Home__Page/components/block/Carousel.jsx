0f068dda9a609c28a6e2e7f04845cb0c3821310a
Abdul Samad <abdulsamadpk9072@gmail.com>
2024-02-15 14:01:16 +0530

    navbar and footer design

--- src/pages/Home__Page/components/block/Carousel.jsx
+++ src/pages/Home__Page/components/block/Carousel.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
 import { HiOutlineArrowRight } from "react-icons/hi2";
+import logo from '../images/logo.png'
+import iphone from '../images/iphone.jpg'
 
 function Carousel() {
   return (
     <div className='mt-6'>
       <div className='bg-black min-h-fit flex items-center justify-between w-[90%] mx-auto py-16'>
         <div className='text-white xl:w-[27%] lg:w-[35%] md:w-[45%] sm:w-[46%] w-[52%] mx-auto'>
-          <div className=''>
-            <img src="" alt="" />
+          <div className='flex gap-2 items-center'>
+            <img src={logo} alt="" className='w-[21%]'/>
             <p className='text-sm mt-3'>iPhone 14 series</p>
           </div>
           <h1 className='md:text-6xl fontbold md:leading-[70px] mt-3 text-3xl'>Up to 10% off Voucher</h1>
           <div className='flex items-center cursor-pointer pb-3 gap-3 mt-3'><p className='underline underline-offset-8'>Shop Now</p> <HiOutlineArrowRight className='' /> </div>
         </div>
         <div className='xl:w-[57%] lg:w-[55%] w-[40%]'>
-          <img src="" alt="" />
+          <img src={iphone} alt="" />
         </div>
       </div>
     </div>
