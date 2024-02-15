abcf0f063037dbf35403453c8dec35603111ec3d
Abdul Samad <abdulsamadpk9072@gmail.com>
2024-02-15 18:50:06 +0530

    Carousel and category

--- src/pages/Home__Page/components/block/Carousel.jsx
+++ src/pages/Home__Page/components/block/Carousel.jsx
@@ -9,14 +9,14 @@ function Carousel() {
       <div className='bg-black flex items-center justify-between w-[90%] mx-auto'>
         <div className='text-white xl:w-[26%] lg:w-[35%] md:w-[45%] sm:w-[46%] w-[48%] mx-auto py-16'>
           <div className='flex gap-2 items-center'>
-            <img src={logo} alt="" className='w-[21%]'/>
+            <img src={logo} alt="" className='w-[21%]' />
             <p className='text-sm mt-3'>iPhone 14 series</p>
           </div>
           <h1 className='md:text-6xl fontbold md:leading-[70px] mt-3 text-3xl'>Up to 10% off Voucher</h1>
           <div className='flex items-center cursor-pointer pb-3 gap-3 mt-3'><p className='underline underline-offset-8'>Shop Now</p> <HiOutlineArrowRight className='' /> </div>
         </div>
         <div className='xl:w-[57%] lg:w-[55%] w-[48%] pb-6'>
-          <img src={iphone} alt="" className='lg:min-h-52 md:min-h-64 min-h-56'/>
+          <img src={iphone} alt="" className='lg:min-h-52 md:min-h-64 min-h-56' />
         </div>
       </div>
     </div>
