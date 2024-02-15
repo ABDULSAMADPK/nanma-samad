ef74026c53b740c7d42aa96ec64d8ac544a5ad71
Abdul Samad <abdulsamadpk9072@gmail.com>
2024-02-15 12:31:10 +0530

    navbar and footer design

--- src/pages/Home__Page/components/block/Carousel.jsx
+++ src/pages/Home__Page/components/block/Carousel.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { HiOutlineArrowRight } from "react-icons/hi2";
 
 function Carousel() {
   return (
-    <div>
-        
+    <div className='mt-6'>
+      <div className='bg-black min-h-fit flex items-center justify-between w-[90%] mx-auto py-16'>
+        <div className='text-white xl:w-[27%] lg:w-[35%] md:w-[45%] sm:w-[46%] w-[52%] mx-auto'>
+          <div className=''>
+            <img src="" alt="" />
+            <p className='text-sm mt-3'>iPhone 14 series</p>
+          </div>
+          <h1 className='md:text-6xl fontbold md:leading-[70px] mt-3 text-3xl'>Up to 10% off Voucher</h1>
+          <div className='flex items-center cursor-pointer pb-3 gap-3 mt-3'><p className='underline underline-offset-8'>Shop Now</p> <HiOutlineArrowRight className='' /> </div>
+        </div>
+        <div className='xl:w-[57%] lg:w-[55%] w-[40%]'>
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
--- src/pages/Home__Page/index.jsx
+++ src/pages/Home__Page/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/UI/Navbar'
 import Footer from '../../components/UI/Footer'
+import Carousel from './components/block/Carousel'
 
 function HomePage() {
 
   return (
     <div>
       <Navbar/>
+      <Carousel/>
       <Footer/>
     </div>
   )
